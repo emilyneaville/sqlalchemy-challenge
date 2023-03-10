a0fbc7e95189b2faede234830bc984dad698574f
emilyneaville <119391399+emilyneaville@users.noreply.github.com>
2023-03-09 22:13:16 -0700

    Added stations route and tested
    Successful return

--- Climate Analysis/app.py
+++ Climate Analysis/app.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
 import datetime as dt
+import numpy as np
 
 #################################################
 # Database Setup
@@ -39,6 +40,7 @@ def welcome():
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations"
     )
 
 
@@ -48,7 +50,7 @@ def precipitation():
     session = Session(engine)
      
     # Query the results from the precipitation analysis
-    results = session.query(measurement.date, measurement.prcp).\
+    precipitation_results = session.query(measurement.date, measurement.prcp).\
             filter(measurement.date >= dt.date(2016, 8, 23)).\
             order_by(measurement.date).all()
      
@@ -57,13 +59,32 @@ def precipitation():
     
     # Return converted dictionary (using date as the key and prcp as the value) as json file
     precipitation_all = []
-    for date, prcp in results:
+    for date, prcp in precipitation_results:
         precipitation_dict = {}
         precipitation_dict['date'] = date
         precipitation_dict['prcp'] = prcp
         precipitation_all.append(precipitation_dict)
         
     return jsonify(precipitation_all)
+
+
+@app.route('/api/v1.0/stations')
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+     
+    # Query a list of stations from the dataset
+    station_results = session.query(station.station).all()
+    
+    # Close the session
+    session.close()
+    
+    # Convert list of tuples into normal list
+    all_stations = list(np.ravel(station_results))
+    
+    # Return list of all stations in JSON format
+    return jsonify(all_stations)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
